6c59f7ea6950b0c60afbaa345020776a6f3314ba
00010665 <student00010665@gmail.com>
2021-03-17 17:18:27 +0500

    Add some pages

--- app.js
+++ app.js
@@ -9,4 +9,19 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(8000)
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.get('/notes', (req, res) => {
+    res.render('notes', {noteList: ['First', 'Second']})
+})
+
+app.get('/notes/detail', (req, res) => {
+    res.render('detail')
+})
+
+app.listen(8000, err => {
+    if (err) console.log(err)
+    console.log("App is running on port 8000 ...")
+})
